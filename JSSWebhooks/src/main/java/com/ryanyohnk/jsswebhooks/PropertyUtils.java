17a125bfe600f6e997f2755374275ed0a82ce9a4
Ryan Yohnk <ryan.yohnk@jamfsoftware.com>
2015-12-15 21:59:21 -0600

    Added the PropertyUtils class. Still need to implement a backing implementation for this.

---
+++ JSSWebhooks/src/main/java/com/ryanyohnk/jsswebhooks/PropertyUtils.java
@@ -0,0 +1,16 @@
+package com.ryanyohnk.jsswebhooks;
+
+import java.net.URI;
+
+//TODO - Pull these from a legit location
+public class PropertyUtils {
+
+	public static int getNumThreads() {
+		return 5;
+	}
+
+	public static URI getPostURL() {
+		return null;
+	}
+
+}
